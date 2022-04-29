04ed498bd6d0c6909f98cb22cda5dc016c78f4fe
momo3038 <morgan.leroi@gmail.com>
2022-04-29 15:40:56 +0200

    fix(script): Better Auth handling

--- scripts/create-new-alert.js
+++ scripts/create-new-alert.js
@@ -1,4 +1,3 @@
-
 // node scripts/create-new-alert.js '{ "resource":"BDD", "event":"Bdd is down" }'
 // node scripts/create-new-alert.js '{ "resource":"API", "event":"Slowness detected" }'
 // node scripts/create-new-alert.js '{ "resource":"WebAPI", "event":"Release in progress ...", "severity":"warning" }'
@@ -6,39 +5,42 @@
 
 const fetch = require('node-fetch');
 const fs = require('fs');
-const path = require('path');
 
-const secretFilePath = "scripts/secrets.json";
+const secretFilePath = 'scripts/secrets.json';
 
 if (!fs.existsSync(secretFilePath)) {
-    fs.writeFileSync(secretFilePath, JSON.stringify({
-        alertaUrl: "http://localhost:8080/api",
-        alertaToken: "XYZ"
-    }));
+  fs.writeFileSync(
+    secretFilePath,
+    JSON.stringify({
+      alertaUrl: 'http://localhost:8080/api',
+      alertaToken: 'XX',
+    }),
+  );
 }
 
-var secretFile = fs.readFileSync(secretFilePath, "utf-8")
+var secretFile = fs.readFileSync(secretFilePath, 'utf-8');
 var secrets = JSON.parse(secretFile);
+console.log(secrets);
 const patch = process.argv.slice(2)?.length > 0 ? JSON.parse(process.argv.slice(2)[0]) : {};
 
 const body = {
-    resource: "webapi",
-    event: "HttpServerError",
-    environment: "Production",
-    service: ["myservice.com"],
-    severity: "major",
-    status: "open",
-    text: "Outage in progress ...",
-    group:"test",
-    ...patch
+  resource: 'webapi',
+  event: 'HttpServerError',
+  environment: 'Production',
+  service: ['myservice.com'],
+  severity: 'major',
+  status: 'open',
+  text: 'Outage in progress ...',
+  group: 'test',
+  ...patch,
 };
 
-console.log(body)
+console.log(body);
 
 fetch(`${secrets.alertaUrl}/alert`, {
-    method: 'post',
-    body: JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json', "Authorization": secrets.alertaToken },
+  method: 'post',
+  body: JSON.stringify(body),
+  headers: { 'Content-Type': 'application/json', Authorization: `Key ${secrets.alertaToken}` },
 })
-    .then(res => res.json())
-    .then(json => console.log(json));
+  .then(res => res.json())
+  .then(json => console.log(json));
